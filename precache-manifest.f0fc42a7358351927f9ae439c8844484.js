da60853ae61412a831360485d063ce3f28958f0d
eyesofdaveed <davdauit@gmail.com>
2021-08-26 10:29:05 +0600

    Updates

---
+++ precache-manifest.f0fc42a7358351927f9ae439c8844484.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f0ecc47dd689126b3737873b7cbcd5b",
+    "url": "/firebase-chat/index.html"
+  },
+  {
+    "revision": "2acdd43147f5dd623a1b",
+    "url": "/firebase-chat/static/css/2.ad76a385.chunk.css"
+  },
+  {
+    "revision": "2d8349f45cc39797b170",
+    "url": "/firebase-chat/static/css/main.37448acd.chunk.css"
+  },
+  {
+    "revision": "2acdd43147f5dd623a1b",
+    "url": "/firebase-chat/static/js/2.26b002ba.chunk.js"
+  },
+  {
+    "revision": "2182b49f32ec797f3b9c375cbe5c7fb3",
+    "url": "/firebase-chat/static/js/2.26b002ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d8349f45cc39797b170",
+    "url": "/firebase-chat/static/js/main.43addd1c.chunk.js"
+  },
+  {
+    "revision": "d65705acf9d2723d5c78",
+    "url": "/firebase-chat/static/js/runtime-main.004e6c73.js"
+  }
+]);
